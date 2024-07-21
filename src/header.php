8069dd0ade70c9a140ad38c56df9a692fcdb3fe7
Pasindu Nethsara <152866219+Pasindu-Nethsara-Weerasuriya@users.noreply.github.com>
2024-07-22 01:37:14 +0530

    Pasindu (#2)
    * Hedare design done 2024/07/21
    
    * header design finalize and done,change colors and adding features

--- src/header.php
+++ src/header.php
@@ -24,15 +24,34 @@
 
     <div>
 
+      <!-- content -->
+
+      <div class="bg-[#645FCE] max-w-full mx-auto p-2 justify-between text-slate-200 flex flex-row fixed top-0 left-0 right-0 items-center">
+
+        <!-- headrelogoandname -->
+
+        <div class="flex flex-row items-center lg:gap-4">
+
+          <img class="h-[48px] sm:ml-3 lg:ml-5 rounded-full" src="resources/logo/logo.png" />
+          <span class="hidden text-2xl text-black logotext lg:block">NeoLife <br/><span class="hidden text-xs text-white logotext lg:block">Pioneers of Digital Era</span></span>
+
       <div class="bg-[#645FCE] max-w-full mx-auto p-2 justify-between text-slate-200 flex flex-row fixed top-0 left-0 right-0 items-center">
 
         <div class="flex flex-row items-center lg:gap-4">
 
           <img class="h-[48px] sm:ml-3 lg:ml-5 rounded-full" src="resources/logo/logo.png" />
           <span class="hidden text-2xl text-black logotext lg:block">NeoLife</span>
+
           <span class="ml-[90px] md:ml-[540px] lg:hidden sm:block logotext">+94&nbsp;&nbsp;775429803</span>
 
         </div>
+
+
+        <!-- headrelogoandname -->
+
+        <!-- navbarcontent -->
+
+
 
         <div>
 
@@ -40,7 +59,11 @@
 
             <a href="#" class="hidden lg:block">
 
+
+              <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-zinc-700 hover:ease-in hover:transition hover:duration-300 hover:text-white">Home</span>
+
               <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-indigo-600 hover:ease-in hover:transition hover:duration-300 hover:text-white">Home</span>
+
 
             </a>
 
@@ -52,37 +75,70 @@
 
             <a href="#" class="hidden lg:block">
 
+
+              <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-zinc-700 hover:ease-in hover:transition hover:duration-300 hover:text-white">Our Services</span>
+
               <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-indigo-600 hover:ease-in hover:transition hover:duration-300 hover:text-white">Our Services</span>
+
 
             </a>
 
             <a href="#" class="hidden lg:block">
+
+
+              <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-zinc-700 hover:ease-in hover:transition hover:duration-300 hover:text-white">Our Project</span>
 
               <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-indigo-600 hover:ease-in hover:transition hover:duration-300 hover:text-white">Our Project</span>
 
+
             </a>
 
             <a href="#" class="hidden lg:block">
+
+
+              <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-zinc-700 hover:ease-in hover:transition hover:duration-300 hover:text-white">About US</span>
 
               <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-indigo-600 hover:ease-in hover:transition hover:duration-300 hover:text-white">About US</span>
 
+
             </a>
 
             <a href="#" class="hidden lg:block">
+
+
+              <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-zinc-700 hover:ease-in hover:transition hover:duration-300 hover:text-white">Contact Us</span>
+
+            </a>
+
+            <!-- dropdown -->
+
 
               <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-indigo-600 hover:ease-in hover:transition hover:duration-300 hover:text-white">Contact Us</span>
 
             </a>
 
+
             <a href="#">
     
               <div class="relative sm:block lg:hidden" id="dropdownButton">
+
+
+                <!-- dropdownbutton -->
+              
+
 
                 <button class="justify-center w-full gap-4 px-4 py-2 text-black transition rounded-md transfor ring-inset ring-2 ring-black" onclick="Dropdown()">
 
                   <i class="text-lg fa-solid fa-bars fa-fade"></i>
 
                 </button>
+
+
+                <!-- dropdownbutton -->
+
+                <!-- dropdowncontent -->
+
+
 
                 <div class="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
 
@@ -98,16 +154,34 @@
                   </div>
 
                 </div>
+
+
+                <!-- dropdowncontent -->
+
+
               
               </div>
 
             </a>
 
+
+            <!-- dropdown -->
+
+
+
           </ul>
 
         </div>
 
+
+        <!-- navbarcontent -->
+
+      </div>
+      
+      <!-- content -->
+
       </div>      
+
 
     </div>
 
