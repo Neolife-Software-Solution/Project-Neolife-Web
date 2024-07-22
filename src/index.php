d7a71b895a9726d5f040d04698aef775bcfdc49e
AVI SH KA <avishkajayalath2003@gmail.com>
2024-07-23 01:59:38 +0530

    linked style.css file,  rename the title & title icon

--- src/index.php
+++ src/index.php
@@ -2,64 +2,72 @@
 
 <html lang="en">
 
-    <head>
+<head>
 
-        <meta charset="UTF-8">
+    <meta charset="UTF-8">
 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" href="tailwind.css">
+    <link rel="stylesheet" href="tailwind.css">
 
-        <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 
-    </head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <body>
 
-        <!-- background Animation -->
+    <title>Neo Life | Home</title>
 
-        <canvas class="absolute"></canvas>
-        
-        <!-- background Animation -->
-            
-        <div class="h-screen">
+</head>
 
-            <div>
+<body>
 
-                <?php
-                
-                    require "header.php";
+    <!-- background Animation -->
 
-                ?>
-            
-            </div>
-        
-            <!-- headerSection -->
+    <canvas class="absolute"></canvas>
 
-            <div class="mt-16">
-                 
-                <div class="relative flex flex-row justify-center">
-                
-                    <div>
-                    
-                        <div class="flex flex-col mt-64">
+    <!-- background Animation -->
 
-                            <p class="text-3xl font-bold text-white">Neolief Is Our Bussiness In the beautiful Method in World </p>
-                            
-                            <button class="mt-5 bg-[#645FCE] px-5 py-2 text-white rounded-full hover:bg-black font-medium duration-700 ease-out hover:ease-in hover:transition hover:duration-300 hover:text-white">About Us</button>
-                        
-                        </div>
-                                                
+    <div class="h-screen">
+
+        <div>
+
+            <?php
+
+            require "header.php";
+
+            ?>
+
+        </div>
+
+        <!-- headerSection -->
+
+        <div class="mt-16">
+
+            <div class="relative flex flex-row justify-center">
+
+                <div>
+
+                    <div class="flex flex-col mt-64">
+
+                        <p class="text-3xl font-bold text-white">Neolief Is Our Bussiness In the beautiful Method in World </p>
+
+                        <button class="mt-5 bg-[#645FCE] px-5 py-2 text-white rounded-full hover:bg-black font-medium duration-700 ease-out hover:ease-in hover:transition hover:duration-300 hover:text-white">About Us</button>
+
                     </div>
 
                 </div>
 
             </div>
-            
-            <!-- headerSection -->
-             
+
         </div>
-                
-    </body>
+
+        <!-- headerSection -->
+
+    </div>
+    <!--Staff Reviwe  -->
+    <div>
+
+    </div>
+</body>
 
 </html>
