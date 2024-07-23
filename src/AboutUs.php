c5cc88e6a13514431e8edeb60b8da45a3e4e7969
Jude <jmalshanmel@gmail.com>
2024-07-24 05:18:02 +0530

    Responsive About Us - Neolife Software Solutions Page. * Vision & Mission Shadow Added. Desige Finalized and Completed By Beta Team (Kavindu,Jude)

--- src/AboutUs.php
+++ src/AboutUs.php
@@ -42,19 +42,19 @@
 
         <!-- body -->
 
-        <div class="mt-20">
+        <div class="mt-28 sm:mt-20">
 
 
             <!-- bodyheading -->
 
             <div class="flex flex-row justify-center">
 
-                <div class="p-10">
+                <div class="sm:p-9 md:p-10">
 
-                    <!-- font is Montserrat-SemiBold -->
-                    <h1 class="text-3xl font-black headertext-aboutus">Neolife Software Solution</h1>
-                    <!-- font is Montserrat-SemiBold -->
-                    
+                    <!-- font is Montserrat-ExtraBold -->
+                    <h1 class="text-3xl font-bold headertext-aboutus text-center lg:text-balance sm:text-wrap">Neolife Software Solution</h1>
+                    <!-- font is Montserrat-ExtraBold -->
+
                 </div>
 
             </div>
@@ -64,25 +64,25 @@
 
             <!-- vision & mission -->
 
-            <div class="flex flex-col px-40">
+            <div class="flex flex-col px-11 sm:px-24 md:px-28">
 
-                <div class="w-full h-38 rounded-3xl text-center p-10 bg-[#645FCE] mt-10">
+                <div class="w-full h-38 rounded-3xl text-center p-10 bg-[#645FCE] hover:bg-[#655fceeb] shadow-[0_35px_60px_-15px_rgb(2,2,2,0.9)] mt-10">
 
-                    <!-- font is Montserrat-SemiBold -->
-                    <h1 class="headertext-aboutus text-center text-4xl font-[1000] ">Vision</h1>
-                    <!-- font is Montserrat-SemiBold -->
+                    <!-- font is Montserrat-ExtraBold -->
+                    <h1 class="headertext-aboutus text-center text-4xl font-black">Vision</h1>
+                    <!-- font is Montserrat-ExtraBold -->
 
                     <p class="mt-3">To be a leading provider of innovative software solutions that empower businesses to
                         achieve their full potential and drive meaningful change in their industries.</p>
 
                 </div>
 
-                <div class="w-full h-38 rounded-3xl text-center p-10 bg-[#645FCE] mt-[72px]">
+                <div class="w-full h-38 rounded-3xl text-center p-10 bg-[#645FCE] hover:bg-[#655fceeb] shadow-[0_35px_60px_-15px_rgb(2,2,2,0.9)] mt-[72px]">
 
-                    <!-- font is Montserrat-SemiBold -->
-                    <h1 class="headertext-aboutus text-center text-4xl font-[1000] ">Mission</h1>
-                    <!-- font is Montserrat-SemiBold -->
-                    
+                    <!-- font is Montserrat-ExtraBold -->
+                    <h1 class="headertext-aboutus text-center text-4xl font-black">Mission</h1>
+                    <!-- font is Montserrat-ExtraBold -->
+
                     <p class="mt-3">To deliver exceptional software solutions that exceed our clients' expectations,
                         foster long-term partnerships, and contribute to the success of businesses worldwide.</p>
 
@@ -97,7 +97,7 @@
 
             <!-- des1 -->
 
-            <div class="grid grid-cols-2 p-40">
+            <div class="grid grid-cols-1 md:grid-cols-2 p-11 sm:px-24 md:p-28 mt-10 sm:mt-16 md:mt-0 pb-0">
 
                 <div class="rounded-3xl">
 
@@ -107,7 +107,7 @@
 
                 <div class="align-middle">
 
-                    <div class="p-20 mt-8 text-lg text-center lg:text-balance sm:text-wrap">
+                    <div class="p-11 sm:p-16 text-lg leading-8 text-center lg:text-balance sm:text-wrap">
 
                         <p><b>Client-Centric Approach:</b> We prioritize our clients' needs and work closely with
                             them to ensure that our solutions align with their business goals.</p>
@@ -120,13 +120,38 @@
 
             <!-- des1 -->
 
-            <!-- des2 -->
+            <!-- des2-small&medium -->
 
-            <div class="grid grid-cols-2 px-40">
+            <div class="grid grid-cols-1 p-11 sm:px-24 md:hidden pb-0 md:pt-0">
+
+                <div class="rounded-3xl">
+
+                    <img class="rounded-3xl" src="./resources/aboutus_imgs/Des2.jpg" alt="Description Image 1" />
+
+                </div>
 
                 <div class="align-middle">
 
-                    <div class="p-20 mt-8 text-lg text-center lg:text-balance sm:text-wrap">
+                    <div class="p-11 sm:p-16 text-lg leading-8 text-center lg:text-balance sm:text-wrap">
+
+                        <p><b>Quality Assurance:</b> Our rigorous quality assurance processes guarantee
+                            that every product we deliver meets the highest standards of performance and reliability.</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- des2-small&medium -->
+
+            <!-- des2-large -->
+
+            <div class="hidden md:grid grid-cols-2 p-24 py-0">
+
+                <div class="align-middle">
+
+                    <div class="p-11 sm:p-16 text-lg leading-8 text-center lg:text-balance sm:text-wrap">
 
                         <p><b>Quality Assurance:</b> Our rigorous quality assurance processes guarantee
                             that every product we deliver meets the highest standards of performance and reliability.</p>
@@ -143,11 +168,12 @@
 
             </div>
 
-            <!-- des2 -->
+            <!-- des2-large -->ෆ
+
 
             <!-- des3 -->
 
-            <div class="grid grid-cols-2 p-40">
+            <div class="grid grid-cols-1 md:grid-cols-2 p-11 sm:px-24 md:p-28 mt-10 sm:mt-0 pb-0">
 
                 <div class="rounded-3xl">
 
@@ -157,7 +183,7 @@
 
                 <div class="align-middle">
 
-                    <div class="p-20 mt-8 text-lg text-center lg:text-balance sm:text-wrap">
+                    <div class="p-11 sm:p-16 text-lg leading-8 text-center lg:text-balance sm:text-wrap">
 
                         <p><b>Dedicated Support:</b> Our commitment to excellent customer service means that we are
                             always available to provide support and assistance whenever you need it.</p>
@@ -170,13 +196,39 @@
 
             <!-- des3 -->
 
-            <!-- des4 -->
+            <!-- des4-small -->
 
-            <div class="grid grid-cols-2 px-40 mb-32">
+            <div class="grid grid-cols-1 p-11 sm:px-24 md:hidden pb-0">
+
+                <div class="rounded-3xl">
+
+                    <img class="rounded-3xl" src="./resources/aboutus_imgs/Des4.jpg" alt="Description Image 4" />
+
+                </div>
 
                 <div class="align-middle">
 
-                    <div class="p-20 mt-8 text-lg text-center lg:text-balance sm:text-wrap">
+                    <div class="p-11 sm:p-16 text-lg leading-8 text-center lg:text-balance sm:text-wrap">
+
+                        <p><b>Innovative Solutions:</b> We leverage the latest technologies and industry best practices
+                            to deliver innovative solutions that drive business growth.</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- des4-small -->
+
+
+            <!-- des4-large -->
+
+            <div class="hidden md:grid grid-cols-2 p-24 pt-0">
+
+                <div class="align-middle">
+
+                    <div class="p-11 sm:p-16 text-lg leading-8 text-center lg:text-balance sm:text-wrap">
 
                         <p><b>Innovative Solutions:</b> We leverage the latest technologies and industry best practices
                             to deliver innovative solutions that drive business growth.</p>
@@ -193,8 +245,7 @@
 
             </div>
 
-            <!-- des4 -->
-
+            <!-- des4-large -->
 
             <!-- description -->
 
