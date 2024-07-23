ecbc8b22f5cda45efbcf9f4fa2314404406a46aa
sanjana-kumara <sanjanakumara92@gmail.com>
2024-07-23 17:41:06 +0530

    Done Background Animation

--- src/index.php
+++ src/index.php
@@ -2,135 +2,80 @@
 
 <html lang="en">
 
-<head>
+    <head>
 
-    <meta charset="UTF-8">
+        <meta charset="UTF-8">
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="tailwind.css">
+        <link rel="stylesheet" href="tailwind.css">
 
+        <link rel="stylesheet" href="style.css">
 
-    <title>Document</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-</head>
+        <title>Neo Life | Home</title>
 
-<body>
+    </head>
 
-    <!-- background Animation -->
+    <body>
 
-    <canvas class="absolute"></canvas>
+        <!-- background Animation -->
 
-    <!-- background Animation -->
+        <canvas class="absolute"></canvas>
 
-    <div class="h-screen">
+        <!-- background Animation -->
 
-        <div>
+        <div class="h-screen">
 
-            <?php
-                
-                require "header.php";
+            <div>
 
-            ?>
+                <?php
+                    
+                    require "header.php";
 
-            <!-- headerSection -->
+                ?>
 
-            <div class="mt-16">
+                <!-- headerSection -->
 
-                <div class="relative flex flex-col justify-center">
+                <div class="mt-16">
 
-                    <div>
+                    <div class="relative flex flex-col justify-center">
 
-                        <div class="flex flex-col mt-32">
+                        <div>
 
-                            <div class="px-5">
+                            <div class="flex flex-col mt-32">
 
-                                <p class="text-xl font-bold text-center text-white">At Neolife Software Solutions, we
-                                    specialize in delivering cutting-edge technology solutions tailored to meet the
-                                    unique needs of businesses across various industries. Our mission is to empower our
-                                    clients with innovative software that enhances productivity, efficiency, and overall
-                                    business performance. </p>
+                                <div class="px-5">
+
+                                    <p class="text-xl font-bold text-center text-white">At Neolife Software Solutions, we
+                                        specialize in delivering cutting-edge technology solutions tailored to meet the
+                                        unique needs of businesses across various industries. Our mission is to empower our
+                                        clients with innovative software that enhances productivity, efficiency, and overall
+                                        business performance. </p>
+
+                                </div>
+
+                                <div class="flex flex-row justify-center">
+
+                                    <button class="mt-5 bg-[#645FCE] px-10 py-2 text-white rounded-full hover:bg-black font-medium duration-700 ease-out hover:ease-in hover:transition hover:duration-300 hover:text-white">About Us</button>
+
+                                </div>
 
                             </div>
-
-                            <div class="flex flex-row justify-center">
-
-                                <button class="mt-5 bg-[#645FCE] px-10 py-2 text-white rounded-full hover:bg-black font-medium duration-700 ease-out hover:ease-in hover:transition hover:duration-300 hover:text-white">About Us</button>
-
-                            </div>
-
+                        
                         </div>
-
-
-    <link rel="stylesheet" href="style.css">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-
-    <title>Neo Life | Home</title>
-
-</head>
-
-<body>
-
-    <!-- background Animation -->
-
-    <canvas class="absolute"></canvas>
-
-    <!-- background Animation -->
-
-    <div class="h-screen">
-
-        <div>
-
-            <?php
-
-            require "header.php";
-
-            ?>
-
-        </div>
-
-        <!-- headerSection -->
-
-        <div class="mt-16">
-
-            <div class="relative flex flex-row justify-center">
-
-                <div>
-
-                    <div class="flex flex-col mt-64">
-
-                        <p class="text-3xl font-bold text-white">Neolief Is Our Bussiness In the beautiful Method in World </p>
-
-                        <button class="mt-5 bg-[#645FCE] px-5 py-2 text-white rounded-full hover:bg-black font-medium duration-700 ease-out hover:ease-in hover:transition hover:duration-300 hover:text-white">About Us</button>
-
-
+                        
                     </div>
-
-                </div>
+                    
+                </div>           
+                
+                <!-- headerSection -->
 
             </div>
 
-            
-            <!-- headerSection -->
-
         </div>
 
-    </div>
-
-
-
-        </div>
-
-        <!-- headerSection -->
-
-    </div>
-    <!--Staff Reviwe  -->
-    <div>
-
-    </div>
-
-</body>
+    </body>
 
 </html>
