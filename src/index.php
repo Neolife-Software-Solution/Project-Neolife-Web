e6c06dfef219f94642a22780b92748c27bf23f7d
sanjana-kumara <sanjanakumara92@gmail.com>
2024-07-24 17:06:12 +0530

    Done Our Services Images Slider

--- src/index.php
+++ src/index.php
@@ -10,9 +10,11 @@
 
         <link rel="stylesheet" href="tailwind.css">
 
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="service_img_slider.scss">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <link rel="icon" href="resources/logo/favicon.png" />
 
         <title>Neo Life | Home</title>
 
@@ -22,7 +24,7 @@
 
         <!-- background Animation -->
 
-        <canvas class="absolute"></canvas>
+        <!-- <canvas class="absolute"></canvas> -->
 
         <!-- background Animation -->
 
@@ -36,19 +38,21 @@
 
                 ?>
 
-                <!-- headerSection -->
-
-                <div class="mt-16">
+                <!-- Our Services Section -->
+                
+                <div class="hidden mt-16 lg:block">
 
                     <div class="relative flex flex-col justify-center">
 
                         <div>
-
-                            <div class="flex flex-col mt-32">
+                            
+                            <!-- background Animation Content -->
+                                 
+                            <div class="flex flex-col mt-32 ">
 
                                 <div class="px-5">
 
-                                    <p class="text-xl font-bold text-center text-white">At Neolife Software Solutions, we
+                                    <p class="text-xl font-bold text-center text-black">At Neolife Software Solutions, we
                                         specialize in delivering cutting-edge technology solutions tailored to meet the
                                         unique needs of businesses across various industries. Our mission is to empower our
                                         clients with innovative software that enhances productivity, efficiency, and overall
@@ -63,18 +67,162 @@
                                 </div>
 
                             </div>
+
+                            <!-- background Animation Content -->
                         
                         </div>
                         
                     </div>
                     
-                </div>           
-                
-                <!-- headerSection -->
+                    <!-- Image Slider -->
+                    
+                    <div class=" mt-60">
+                    
+                        <div class="overflow-hidden">
+                            
+                            <div class="bg-[#645FCE] py-5 relative mt-80">
+                            
+                                <div>
+            
+                                    <span class="relative flex flex-row justify-center text-2xl font-bold text-white headertext">Our Services</span>
+            
+                                </div>
+            
+                                
+                            </div>
+                            
+                            <div class="container_img">
 
+                                <div>
+
+                                    <div class="w-screen">
+                                    
+                                        <!-- images -->
+                                        <div class="slide">
+                                            
+                                            <div class="item" style="background-image: url(resources/Services_img_slider/Application\ Development\(2\).jpeg);">
+
+                                                <div class="content">
+
+                                                    <div class="name">Application Development</div>
+                                                    
+                                                    <div class="des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempore error obcaecati. Odit quas autem, id architecto, facere pariatur delectus iusto recusandae officia tempore, atque impedit nostrum necessitatibus libero eos!</div>
+                                                    
+                                                    <button class="px-10 py-2 text-black bg-white btn">See More</button>
+                                                    
+                                                </div>
+
+                                            </div>
+                                            <div class="bg-cover item h-80" style="background-image: url(resources/Services_img_slider/web-development\(1\).jpg);">
+
+                                                <div class="content">
+
+                                                    <div class="name">web Development</div>
+
+                                                    <div class="des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempore error obcaecati. Odit quas autem, id architecto, facere pariatur delectus iusto recusandae officia tempore, atque impedit nostrum necessitatibus libero eos!</div>
+                                                    
+                                                    <button class="px-10 py-2 text-black bg-white btn">See More</button>
+                                                    
+                                                </div>
+
+                                            </div>
+                                            <div class="bg-cover item h-80" style="background-image: url(resources/Services_img_slider/System\ Development\(1\).webp);">
+
+                                                <div class="content">
+
+                                                    <div class="name">System Development</div>
+                                                    
+                                                    <div class="des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempore error obcaecati. Odit quas autem, id architecto, facere pariatur delectus iusto recusandae officia tempore, atque impedit nostrum necessitatibus libero eos!</div>
+                                                    
+                                                    <button class="px-10 py-2 text-black bg-white btn">See More</button>
+                                                    
+                                                </div>
+
+                                            </div>
+                                            <div class="bg-cover item h-80" style="background-image: url(resources/Services_img_slider/Graphi\ Design\ \(3\).png);">
+
+                                                <div class="content">
+
+                                                    <div class="name">Graphic Design</div>
+                                                    
+                                                    <div class="des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempore error obcaecati. Odit quas autem, id architecto, facere pariatur delectus iusto recusandae officia tempore, atque impedit nostrum necessitatibus libero eos!</div>
+                                                    
+                                                    <button class="px-10 py-2 text-black bg-white btn">See More</button>
+                                                    
+                                                </div>
+
+                                            </div>
+                                            <div class="bg-cover item h-80" style="background-image: url(resources/Services_img_slider/Video\ Editing\(1\).webp);">
+
+                                                <div class="content">
+
+                                                    <div class="name">Video Editing</div>
+
+                                                    <div class="des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempore error obcaecati. Odit quas autem, id architecto, facere pariatur delectus iusto recusandae officia tempore, atque impedit nostrum necessitatibus libero eos!</div>
+                                                
+                                                    <button class="px-10 py-2 text-black bg-white btn">See More</button>
+                                                    
+                                                </div>
+
+                                            </div>
+                                            <div class="bg-cover item h-80" style="background-image: url(resources/Services_img_slider/computer_hardware_\(3\).png);">
+
+                                                <div class="content">
+
+                                                    <div class="name">computer Hardware</div>
+                                                    
+                                                    <div class="des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempore error obcaecati. Odit quas autem, id architecto, facere pariatur delectus iusto recusandae officia tempore, atque impedit nostrum necessitatibus libero eos!</div>
+                                                    
+                                                    <button class="px-10 py-2 text-black bg-white btn">See More</button>
+                                                    
+                                                </div>
+
+                                            </div>
+                                            <div class="bg-cover item h-80" style="background-image: url(resources/Services_img_slider/computer_networking.png);">
+
+                                                <div class="content">
+
+                                                    <div class="name">Networking</div>
+                                                    
+                                                    <div class="des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempore error obcaecati. Odit quas autem, id architecto, facere pariatur delectus iusto recusandae officia tempore, atque impedit nostrum necessitatibus libero eos!</div>
+                                                    
+                                                    <button class="px-10 py-2 text-black bg-white btn">See More</button>
+                                                    
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+                                        <!-- images -->
+                                        
+                                    </div>
+
+                                </div>
+                                
+                                <div class="button">
+
+                                    <button class="prev btn2"><i class="fa-solid fa-arrow-left"></i></button>                                
+                                    <button class="next btn2"><i class="fa-solid fa-arrow-right"></i></button>
+
+                                </div>
+
+                            </div>
+                                    
+                        </div>
+                        
+                    </div>
+                    
+                    <!-- Image Slider -->
+                                       
+                </div> 
+                
+                <!-- Our Services Section -->
+                
             </div>
 
         </div>
+
+        <script src="service_img_slider_animation.js"></script>
 
     </body>
 
--- src/tailwind.js
+++ src/tailwind.js
@@ -6,109 +6,134 @@ function Dropdown() {
 
 }
 
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
+// Home Page background Animation 
 
-onload = function (){
-  setTimeout(init,0)
-}
+// window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
 
-init = function(){
-  canvas = document.querySelector('canvas')
-  ctx = canvas.getContext('2d')
+// onload = function (){
+//   setTimeout(init,0)
+// }
 
-  onresize = function(){
-    canvas.width = canvas.clientWidth
-    canvas.height = canvas.clientHeight
-  }
-  onresize()
+// init = function(){
+//   canvas = document.querySelector('canvas')
+//   ctx = canvas.getContext('2d')
 
-  mouse = {x:canvas.width/2,y:canvas.height/2,out:false}
+//   onresize = function(){
+//     canvas.width = canvas.clientWidth
+//     canvas.height = canvas.clientHeight
+//   }
+//   onresize()
 
-  canvas.onmouseout = function(){
-    mouse.out = true
-  }
+//   mouse = {x:canvas.width/2,y:canvas.height/2,out:false}
 
-  canvas.onmousemove = function(e){
-    var rect = canvas.getBoundingClientRect()
-    mouse = {
-      x: e.clientX - rect.left,
-      y: e.clientY - rect.top,
-      out: false
-    }
-  }
+//   canvas.onmouseout = function(){
+//     mouse.out = true
+//   }
 
-  gravityStrength = 10
-  particles = []
-  spawnTimer = 0
-  spawnInterval = 10
-  type = 0
-  requestAnimationFrame(startLoop)
-}
+//   canvas.onmousemove = function(e){
+//     var rect = canvas.getBoundingClientRect()
+//     mouse = {
+//       x: e.clientX - rect.left,
+//       y: e.clientY - rect.top,
+//       out: false
+//     }
+//   }
 
-newParticle = function(){
-  type = type?0:1
-  particles.push({
-    x:mouse.x,
-    y:mouse.y,
-    xv:type?18*Math.random()-9:24*Math.random()-12,
-    yv:type?18*Math.random()-9:24*Math.random()-12,
-    c:type?'rgb(255,'+((200*Math.random())|0)+','+((80*Math.random())|0)+')':'rgb(255,255,255)',
-    s:type?5+10*Math.random():1,
-    a:1
-  })
-}
+//   gravityStrength = 10
+//   particles = []
+//   spawnTimer = 0
+//   spawnInterval = 10
+//   type = 0
+//   requestAnimationFrame(startLoop)
+// }
 
-startLoop = function(newTime){
-  time = newTime
-  requestAnimationFrame(loop)
-}
+// newParticle = function(){
+//   type = type?0:1
+//   particles.push({
+//     x:mouse.x,
+//     y:mouse.y,
+//     xv:type?18*Math.random()-9:24*Math.random()-12,
+//     yv:type?18*Math.random()-9:24*Math.random()-12,
+//     c:type?'rgb(255,'+((200*Math.random())|0)+','+((80*Math.random())|0)+')':'rgb(255,255,255)',
+//     s:type?5+10*Math.random():1,
+//     a:1
+//   })
+// }
 
-loop = function(newTime){
-  draw()
-  calculate(newTime)
-  requestAnimationFrame(loop)
-}
+// startLoop = function(newTime){
+//   time = newTime
+//   requestAnimationFrame(loop)
+// }
 
-draw = function(){
-  ctx.clearRect(0,0,canvas.width,canvas.height)
-  for(var i=0;i<particles.length;i++){
-    var p = particles[i]
-    ctx.globalAlpha = p.a
-    ctx.fillStyle = p.c
-    ctx.beginPath()
-    ctx.arc(p.x,p.y,p.s,0,2*Math.PI)
-    ctx.fill()
-  }
-}
+// loop = function(newTime){
+//   draw()
+//   calculate(newTime)
+//   requestAnimationFrame(loop)
+// }
 
-calculate = function(newTime){
-  var dt = newTime-time
-  time = newTime
+// draw = function(){
+//   ctx.clearRect(0,0,canvas.width,canvas.height)
+//   for(var i=0;i<particles.length;i++){
+//     var p = particles[i]
+//     ctx.globalAlpha = p.a
+//     ctx.fillStyle = p.c
+//     ctx.beginPath()
+//     ctx.arc(p.x,p.y,p.s,0,2*Math.PI)
+//     ctx.fill()
+//   }
+// }
 
-  if(!mouse.out){
-    spawnTimer += (dt<100)?dt:100
-    for(;spawnTimer>0;spawnTimer-=spawnInterval){
-      newParticle()
-    }
-  }
+// calculate = function(newTime){
+//   var dt = newTime-time
+//   time = newTime
 
-  particleOverflow = particles.length-700
-  if(particleOverflow>0){
-    particles.splice(0,particleOverflow)
-  }
+//   if(!mouse.out){
+//     spawnTimer += (dt<100)?dt:100
+//     for(;spawnTimer>0;spawnTimer-=spawnInterval){
+//       newParticle()
+//     }
+//   }
 
-  for(var i=0;i<particles.length;i++){
-    var p = particles[i]
-    if(!mouse.out){
-      x = mouse.x-p.x
-      y = mouse.y-p.y
-      a = x*x+y*y
-      a = a>100?gravityStrength/a:gravityStrength/100
-      p.xv = (p.xv+a*x)*0.99
-      p.yv = (p.yv+a*y)*0.99
-    }
-    p.x += p.xv
-    p.y += p.yv
-    p.a *= 0.99
-  }
-}
+//   particleOverflow = particles.length-700
+//   if(particleOverflow>0){
+//     particles.splice(0,particleOverflow)
+//   }
+
+//   for(var i=0;i<particles.length;i++){
+//     var p = particles[i]
+//     if(!mouse.out){
+//       x = mouse.x-p.x
+//       y = mouse.y-p.y
+//       a = x*x+y*y
+//       a = a>100?gravityStrength/a:gravityStrength/100
+//       p.xv = (p.xv+a*x)*0.99
+//       p.yv = (p.yv+a*y)*0.99
+//     }
+//     p.x += p.xv
+//     p.y += p.yv
+//     p.a *= 0.99
+//   }
+// }
+
+// Home Page background Animation 
+
+// Our Services Image Slider
+
+// let next = document.querySelector('.next');
+// let prev = document.querySelector('.prev');
+
+// next.addEventListener('click', function () {
+
+//     let items = document.querySelectorAll('.item');
+//     document.querySelector('.slide').appendChild(items[0]);
+
+// });
+
+// prev.addEventListener('click', function () {
+
+//     let items = document.querySelectorAll('.item');
+//     document.querySelector('.slide').prepend(items[items.length - 1]);
+
+// });
+
+// Our Services Image Slider
