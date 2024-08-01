50bf1538ee894708d81261504548a914de0afe02
Pasindu Nethsara <pnethsara32@gmail.com>
2024-08-01 17:03:23 +0530

    create new folder for user and add details in to it

---
+++ user/src/Estimate.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="style.css" />
+
+        <link rel="stylesheet" href="tailwind.css" />
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <link rel="icon" href="resources/logo/favicon.png" />
+
+        <title>Estimate | NeoLife Software Solutions</title>
+
+    </head>
+    
+    <body>
+    
+        <div class="h-screen">
+
+            <div>
+
+                <?php
+                
+                    require "header.php";
+
+                ?>
+
+            </div>            
+            
+            <div>                
+                
+                <div class="">
+                    
+                    <!-- packageselectsection -->
+                     
+                    <div class="mt-16">
+
+                        <div class="py-5 bg-white">
+
+                            <div>
+
+                                <span class="flex flex-row justify-center text-2xl font-bold headertext">
+                                    
+                                    Our Services
+                                    
+                                </span>
+
+                            </div>
+
+                        </div>
+                        
+                        <!-- packages -->
+
+                        <div class="">
+
+                            <div class="">
+
+                                <div class="flex flex-row justify-center mt-5 ">
+
+                                    <div class="relative px-5 py-2 mb-2 bg-white rounded-md shadow-lg tab">
+                                        
+                                        <input type="radio" name="faq" id="faq1" class="appearance-none peer">
+                                        
+                                        <label for="faq1" class="flex items-center cursor-pointer font-semibold text-lg after:content-['+'] after:absolute after:right-5 after:text-2xl     after:text-gray-400 hover:after:text-gray-950 peer-checked:after:transform peer-checked:after:rotate-45">
+                                            
+                                            <h2 class="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-sm bg-[#645FCE]">01</h2>
+                                            
+                                            <h3>Web Development</h3>
+                                            
+                                        </label>
+                                        
+                                        <div class="h-0 mt-5 overflow-hidden transition-all duration-500 ease-in-out answer content peer-checked:h-full lg:w-[1111px]">
+                                            
+                                            <p>
+                                            We strive to begin working on new projects as quickly as possible. Typically, we start within two weeks of receiving a project. However, this timeframe can vary depending on the complexity, requirements, and scope of the project.
+                                            </p>
+
+                                        </div>
+                                        
+                                    </div>
+
+                                </div>
+                                
+                                <div class="flex flex-row justify-center mt-5">
+
+                                    <div class="relative px-5 py-2 mb-2 bg-white rounded-md shadow-lg tab">
+                                        
+                                        <input type="radio" name="faq" id="faq2" class="appearance-none peer">
+                                        
+                                        <label for="faq2" class="flex items-center cursor-pointer font-semibold text-lg after:content-['+'] after:absolute after:right-5 after:text-2xl     after:text-gray-400 hover:after:text-gray-950 peer-checked:after:transform peer-checked:after:rotate-45">
+                                            
+                                            <h2 class="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-sm bg-[#645FCE]">02</h2>
+                                            
+                                            <h3>System Development</h3>
+                                            
+                                        </label>
+                                        
+                                        <div class="h-0 mt-5 overflow-hidden transition-all duration-500 ease-in-out answer content peer-checked:h-full lg:w-[1111px]">
+                                            
+                                            <p>
+                                            Quality is our top priority. To ensure this, we use a short iteration model that allows for regular testing of our work. We make improvements and adjustments based on customer feedback, preferences, and the response from the target audience. This approach ensures that we deliver high-quality products that meet your business needs and audience expectations.
+                                            </p>
+
+                                        </div>
+                                        
+                                    </div>
+
+                                </div>
+                                
+                                <div class="flex flex-row justify-center mt-5">
+
+                                    <div class="relative px-5 py-2 mb-2 bg-white rounded-md shadow-lg tab">
+                                        
+                                        <input type="radio" name="faq" id="faq3" class="appearance-none peer">
+                                        
+                                        <label for="faq3" class="flex items-center cursor-pointer font-semibold text-lg after:content-['+'] after:absolute after:right-5 after:text-2xl     after:text-gray-400 hover:after:text-gray-950 peer-checked:after:transform peer-checked:after:rotate-45">
+                                            
+                                            <h2 class="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-sm bg-[#645FCE]">03</h2>
+                                            
+                                            <h3>application Development</h3>
+                                            
+                                        </label>
+                                        
+                                        <div class="h-0 mt-5 overflow-hidden transition-all duration-500 ease-in-out answer content peer-checked:h-full lg:w-[1111px]">
+                                            
+                                            <p>
+                                            We manage projects using Agile methodologies, which emphasize flexibility, regular communication, and the timely delivery of high-quality software solutions.
+                                            </p>
+
+                                        </div>
+                                        
+                                    </div>
+
+                                </div>
+                                
+                                <div class="flex flex-row justify-center mt-5">
+
+                                    <div class="relative px-5 py-2 mb-2 bg-white rounded-md shadow-lg tab">
+                                        
+                                        <input type="radio" name="faq" id="faq4" class="appearance-none peer">
+                                        
+                                        <label for="faq4" class="flex items-center cursor-pointer font-semibold text-lg after:content-['+'] after:absolute after:right-5 after:text-2xl     after:text-gray-400 hover:after:text-gray-950 peer-checked:after:transform peer-checked:after:rotate-45">
+                                            
+                                            <h2 class="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-sm bg-[#645FCE]">04</h2>
+                                            
+                                            <h3>Hardware and Networking</h3>
+                                            
+                                        </label>
+                                        
+                                        <div class="h-0 mt-5 overflow-hidden transition-all duration-500 ease-in-out answer content peer-checked:h-full lg:w-[1111px]">
+                                            
+                                            <p>
+                                            We specialize in developing custom software solutions, including web applications, mobile apps, and enterprise software tailored to meet specific business needs.
+                                            </p>
+
+                                        </div>
+                                        
+                                    </div>
+
+                                </div>
+                            
+                            </div>
+                                                
+
+                        </div>
+
+                        <!-- packages -->
+
+                    </div>
+                    
+                    <!-- packageselectsection -->
+
+                </div>
+
+            </div>
+
+            <div>
+
+                <?php
+                
+                    require "footer.php";
+
+                ?>
+
+            </div>
+
+        </div>
+    
+    </body>
+
+</html>
---
+++ user/src/test1.php
@@ -0,0 +1,142 @@
+<!-- FAQ Section -->
+
+<div>
+
+    <div>
+
+        <div class="bg-[#645FCE] py-5 mt-10">
+
+            <div>
+
+                <span class="flex flex-row justify-center text-2xl font-bold text-white headertext">
+                    
+                    FAQ
+                    
+                </span>
+
+            </div>
+
+        </div>
+        
+        <!-- Question -->
+
+        <div class="">
+
+            <div class="">
+
+                <div class="flex flex-row justify-center mt-5 ">
+
+                    <div class="relative px-5 py-2 mb-2 bg-white rounded-md shadow-lg tab">
+                        
+                        <input type="radio" name="faq" id="faq1" class="appearance-none peer">
+                        
+                        <label for="faq1" class="flex items-center cursor-pointer font-semibold text-lg after:content-['+'] after:absolute after:right-5 after:text-2xl     after:text-gray-400 hover:after:text-gray-950 peer-checked:after:transform peer-checked:after:rotate-45">
+                            
+                            <h2 class="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-sm bg-[#645FCE]">01</h2>
+                            
+                            <h3>How long does it take before you start working on my project?</h3>
+                            
+                        </label>
+                        
+                        <div class="h-0 mt-5 overflow-hidden transition-all duration-500 ease-in-out answer content peer-checked:h-full lg:w-[1111px]">
+                            
+                            <p>
+                            We strive to begin working on new projects as quickly as possible. Typically, we start within two weeks of receiving a project. However, this timeframe can vary depending on the complexity, requirements, and scope of the project.
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                </div>
+                
+                <div class="flex flex-row justify-center mt-5">
+
+                    <div class="relative px-5 py-2 mb-2 bg-white rounded-md shadow-lg tab">
+                        
+                        <input type="radio" name="faq" id="faq2" class="appearance-none peer">
+                        
+                        <label for="faq2" class="flex items-center cursor-pointer font-semibold text-lg after:content-['+'] after:absolute after:right-5 after:text-2xl     after:text-gray-400 hover:after:text-gray-950 peer-checked:after:transform peer-checked:after:rotate-45">
+                            
+                            <h2 class="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-sm bg-[#645FCE]">02</h2>
+                            
+                            <h3>How do you guarantee product quality?</h3>
+                            
+                        </label>
+                        
+                        <div class="h-0 mt-5 overflow-hidden transition-all duration-500 ease-in-out answer content peer-checked:h-full lg:w-[1111px]">
+                            
+                            <p>
+                            Quality is our top priority. To ensure this, we use a short iteration model that allows for regular testing of our work. We make improvements and adjustments based on customer feedback, preferences, and the response from the target audience. This approach ensures that we deliver high-quality products that meet your business needs and audience expectations.
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                </div>
+                
+                <div class="flex flex-row justify-center mt-5">
+
+                    <div class="relative px-5 py-2 mb-2 bg-white rounded-md shadow-lg tab">
+                        
+                        <input type="radio" name="faq" id="faq3" class="appearance-none peer">
+                        
+                        <label for="faq3" class="flex items-center cursor-pointer font-semibold text-lg after:content-['+'] after:absolute after:right-5 after:text-2xl     after:text-gray-400 hover:after:text-gray-950 peer-checked:after:transform peer-checked:after:rotate-45">
+                            
+                            <h2 class="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-sm bg-[#645FCE]">03</h2>
+                            
+                            <h3>What is your approach to project management?</h3>
+                            
+                        </label>
+                        
+                        <div class="h-0 mt-5 overflow-hidden transition-all duration-500 ease-in-out answer content peer-checked:h-full lg:w-[1111px]">
+                            
+                            <p>
+                            We manage projects using Agile methodologies, which emphasize flexibility, regular communication, and the timely delivery of high-quality software solutions.
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                </div>
+                
+                <div class="flex flex-row justify-center mt-5">
+
+                    <div class="relative px-5 py-2 mb-2 bg-white rounded-md shadow-lg tab">
+                        
+                        <input type="radio" name="faq" id="faq4" class="appearance-none peer">
+                        
+                        <label for="faq4" class="flex items-center cursor-pointer font-semibold text-lg after:content-['+'] after:absolute after:right-5 after:text-2xl     after:text-gray-400 hover:after:text-gray-950 peer-checked:after:transform peer-checked:after:rotate-45">
+                            
+                            <h2 class="flex items-center justify-center w-8 h-8 mr-3 text-white rounded-sm bg-[#645FCE]">04</h2>
+                            
+                            <h3>What types of software do you specialize in developing?</h3>
+                            
+                        </label>
+                        
+                        <div class="h-0 mt-5 overflow-hidden transition-all duration-500 ease-in-out answer content peer-checked:h-full lg:w-[1111px]">
+                            
+                            <p>
+                            We specialize in developing custom software solutions, including web applications, mobile apps, and enterprise software tailored to meet specific business needs.
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                </div>
+            
+            </div>
+                                
+
+        </div>
+
+        <!-- Question -->
+
+    </div>
+
+</div>
+
+<!-- F&Q Section -->
