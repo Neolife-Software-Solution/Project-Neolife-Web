d1d707fce78595073b2b85d76dde4f2be04b4b3f
sanjana-kumara <sanjanakumara92@gmail.com>
2024-08-02 23:52:40 +0530

    Make A Admin Login 01 (Add background Img)

---
+++ admin/src/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="admin_style.css">
+
+        <link rel="stylesheet" href="tailwind.css">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <link rel="icon" href="resources/logo/favicon.png" />
+
+        <title>NeoLife Software Solution | Admin Login</title>
+
+    </head>
+
+    <body class="bg_img">
+
+        <div class="h-screen">
+            
+            <div class="flex flex-row justify-center p-5">
+
+                <div class="flex flex-col">
+
+                    <div class="flex flex-row justify-center">
+                        
+                        <div class="grid grid-cols-1 mt-10 mb-5">
+    
+                            <span class="text-4xl headertext-aboutus">Welcome To Admin Login</span>
+    
+                        </div>
+                    
+                    </div>
+
+                    <div class="flex flex-col mt-2">
+
+                        <div class="grid grid-cols-1">
+
+                            <div class="flex flex-row p-5 shadow-xl bg-slate-700/30 shadow-black/10 rounded-xl">
+    
+                                <div class="">
+    
+                                    <div>
+    
+                                        <input class="h-10 p-5 rounded-full shadow-md w-80 focus:outline-none hover:shadow-black" placeholder="Enter Your Admin Email Address" type="text" name="" id=""/>
+                                    
+                                    </div>
+                                
+                                    <div class="flex justify-end mt-5 ms-40">
+    
+                                        <button class="h-10 py-2 bg-white rounded-full w-52 hover:bg-[#645FCE] font-bold">Get verification Code</button>
+    
+                                    </div>
+
+                                    <div class="mt-10">
+    
+                                        <div >
+    
+                                            <div>
+    
+                                                <span class="flex flex-row justify-center h-10 w-80 bg-[#645FCE]">Verfy Your Email Address</span>
+    
+                                            </div>
+    
+                                        </div>    
+    
+                                    </div>
+                                        
+                                </div>
+    
+                            </div>
+                        
+                        </div>
+                    
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </body>
+
+</html>
