be6891ca174f1f0d94dcfae98903fdd9f4a31917
WG-Kavindu <2003kavinduvishmitha@gmail.com>
2024-08-06 17:49:13 +0530

    AboutUs and OurProject Design Fully Completed (Responsive)

--- user/src/header.php
+++ user/src/header.php
@@ -64,13 +64,13 @@
 
             </a>
 
-            <a href="#" class="hidden lg:block">
+            <a href="OurProject.php" class="hidden lg:block">
 
               <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-zinc-700 hover:ease-in hover:transition hover:duration-300 hover:text-white">Our Project</span>
 
             </a>
 
-            <a href="#" class="hidden lg:block">
+            <a href="AboutUs.php" class="hidden lg:block">
 
               <span class="px-4 py-3 font-medium duration-700 ease-out rounded-md headertext hover:bg-zinc-700 hover:ease-in hover:transition hover:duration-300 hover:text-white">About US</span>
 
@@ -107,8 +107,8 @@
                     <a href="index.php" class="p-3 text-black duration-700 ease-out rounded-md hover:duration-500 hover:text-white hover:bg-indigo-600 hover:ease-in hover:transition">Home</a>
                     <a href="#" class="p-3 text-black duration-700 ease-out rounded-md hover:duration-500 hover:text-white hover:bg-black hover:ease-in hover:transition">Build Your Dream&nbsp;<i class="fa-solid fa-arrow-right fa-shake"></i></a>
                     <a href="#" class="p-3 text-black duration-700 ease-out rounded-md hover:duration-500 hover:text-white hover:bg-indigo-600 hover:ease-in hover:transition">Our Services</a>
-                    <a href="#" class="p-3 text-black duration-700 ease-out rounded-md hover:duration-500 hover:text-white hover:bg-indigo-600 hover:ease-in hover:transition">Our Projects</a>
-                    <a href="#" class="p-3 text-black duration-700 ease-out rounded-md hover:duration-500 hover:text-white hover:bg-indigo-600 hover:ease-in hover:transition">About Us</a>
+                    <a href="OurProject.php" class="p-3 text-black duration-700 ease-out rounded-md hover:duration-500 hover:text-white hover:bg-indigo-600 hover:ease-in hover:transition">Our Projects</a>
+                    <a href="AboutUs.php" class="p-3 text-black duration-700 ease-out rounded-md hover:duration-500 hover:text-white hover:bg-indigo-600 hover:ease-in hover:transition">About Us</a>
                     <a href="ContactUs.php" target="_self" class="p-3 text-black duration-700 ease-out rounded-md hover:duration-500 hover:text-white hover:bg-indigo-600 hover:ease-in hover:transition">Contact Us</a>
 
                   </div>
